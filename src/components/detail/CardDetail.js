96c6d8e57e7355db344570de640a14055c8d905e
jsilva <silvajonatanivan@gmail.com>
2021-11-20 05:42:56 -0300

    Change lenguage and add styles

--- src/components/detail/CardDetail.js
+++ src/components/detail/CardDetail.js
@@ -1,39 +1,38 @@
-import {Loading} from '../loading/Loading';
+import { Loading } from '../loading/Loading';
+import s from './cardDetail.module.css';
 
-export const CardDetail = ({item}) => {
+
+export const CardDetail = ({ item }) => {
   return (
     <>
-      {!item ?  <Loading />
-      : <div>
-        <h1>{item.title}</h1>
-        <img src={item.img} alt={item.title} />
-        {
-          item.types.map(type => <span key={type}>{type}</span>)
-        }
-        <p>Puntuacion: {item.score}</p>
-        <p>Nivel de saludable{item.healthScore}</p>
-        <p>Resumen:<br />{item.summary}</p>
-        <p>{JSON.stringify(item.steps)}</p>
-        {
-          item.steps.map(step => {
-            return (
-              <>
-                <h3>Paso N° {step.number}</h3>
-                <p>{step.step}</p>
-                <span>Los ingredientes:</span>
-                {
-                  step.ingredients.map(ingredient => <span key={ingredient}>{ingredient}</span>)
-                }
-                <span>Los materiales:</span>
-                {
-                  step.equipments.map(equipment => <span key={equipment}>{equipment}</span>)
-                }
-              </>
-            );
-          })
-        }
-      </div>
- 
+      {!item
+        ? <Loading />
+        : <div className={s.container}>
+            <div className={s.uno}>
+              <h1 className={s.title}>{item.title}</h1>
+              <span>#{item.types.join(' #')}</span>
+              <p>Score: {item.score}</p>
+              <p>Health score: {item.healthScore}</p>
+            </div>
+            <div className={s.dos}>
+              <img src={item.img} alt={item.title} className={s.img}/>
+              <p>Summary:<br />{item.summary}</p>
+            </div>
+            <div className={s.tres}>
+            {
+              item.steps.map(step => {
+                return (
+                  <div key={step.number}>
+                    <h3>Step N° {step.number}</h3>
+                    <p>{step.step}</p>
+                    <p>Ingredients required: {step.ingredients.map(i => i.name).join(', ')}</p>
+                    <p>Equipment required: {step.equipments.map(e => e.name).join(', ')}</p>
+                  </div>
+                );
+              })
+            }
+            </div>
+          </div>
       }
     </>
   );
