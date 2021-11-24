622bb8fcf9e1061b96dc43fe10c376e6768a03b1
jsilva <silvajonatanivan@gmail.com>
2021-11-24 19:57:22 -0300

    Clear order and filter at unmount main component

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,15 +1,15 @@
 import { Cards } from './Cards';
 import { Header } from '../header/Header';
 import { useDispatch, useSelector } from 'react-redux';
-import { clearError, getItems } from '../../actions';
+import { clearError, getItems, orderBy, filterBy } from '../../actions';
 import { useEffect, useState } from 'react';
 import { Error } from '../error/Error';
 
 export const Main = () => {
 
   const recipes = useSelector((state) => state.items);
-  const filterBy = useSelector((state) => state.filterBy);
-  const orderBy = useSelector((state) => state.orderBy);
+  const filter = useSelector((state) => state.filterBy);
+  const order = useSelector((state) => state.orderBy);
   const error = useSelector((state) => state.error);
   const dispatch = useDispatch();
   const [pageSelected, setPageSelected] = useState(1);
@@ -17,14 +17,18 @@ export const Main = () => {
   // Get items at mount the component
   useEffect(() => {
     dispatch(getItems());
-    return dispatch(clearError());
+    return () => {
+      dispatch(clearError());
+      dispatch(orderBy('default'));
+      dispatch(filterBy('default'));
+    };
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return (
     <>
       <Header />
       {error && <Error error={error}/>}
-      <Cards items={recipes} filter={filterBy} order={orderBy} pageSelected={pageSelected} setPageSelected={setPageSelected} />
+      <Cards items={recipes} filter={filter} order={order} pageSelected={pageSelected} setPageSelected={setPageSelected} />
     </>
   );
 };
